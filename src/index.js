2e34157b43f79d9e17ea4792080115dc59bedc02
chronicSurfer <kyle.umbarger@gmail.com>
2018-10-30 11:12:25 -0700

    React_Intro 1 | Kyle Umbarger

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+const luckyNumber = () => Math.floor(Math.random()*1000)+1;
+const user = {
+    name: 'Kyle',
+    luckyNumber: luckyNumber()
+};
+
+function greeting (userObj) {
+    console.log(userObj);
+    const userObject = (
+        <div className="container">
+            <h1>Hello, {userObj.name}!</h1>
+            <h2 className="text-muted">Your lucky number is {userObj.luckyNumber}!</h2>
+        </div>
+    );
+    return userObject;
+}
+
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );
